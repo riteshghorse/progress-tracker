22c35cba6519bb5febbdc4c42a29eae2b6bce0ce
riteshghorse <riteshghorse@gmail.com>
2017-10-18 20:43:29 +0530

    post with angular js

--- progress/todos/urls.py
+++ progress/todos/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	url(r'^adding$', views.adding, name='adding'),
+	url(r'^newEntry$', views.newEntry, name='newEntry'),
 ]
--- progress/todos/views.py
+++ progress/todos/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+import json
+from django.shortcuts import render, redirect
+from django.db import connection
+from django.views.decorators.csrf import csrf_protect ,csrf_exempt
+
 
 def index(request):
 	return render(request, 'index.html', {})
 
-def adding(request):
-	print(request.POST.data)
+@csrf_exempt
+def newEntry(request):
+	data = request.body.decode('utf-8') 
+	received_json_data = json.loads(data)
+	print(received_json_data['info'])
+	# data = request.json['info1']
+	# print(data)
+	# item = data
+	# print(item)
 	return render(request, 'index.html', {})
